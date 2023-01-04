2fb91ab44483b7bd5e2fafa178b9f0d8441039fe
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-01-05 01:32:54 +0900

    피라미드출력했드앙

--- 피라미드출력.c
+++ 피라미드출력.c
@@ -4,8 +4,11 @@ int main(void){
     int input;
     printf("정수를 입력해주세요 : ");
     scanf("%d", &input);
-    for(int i=0; i<=input-1; i++){
-        for (int j=0; j<=i; j++){
+    for(int i=0; i<input; i++){
+        for (int j=i; j<input-1; j++){
+            printf(" ");
+        }
+        for (int k=0; k<i*2+1; k++){
             printf("*");
         }
     printf("\n");
