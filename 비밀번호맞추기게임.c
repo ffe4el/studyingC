b5ce21a8c80b0a406e6d724dc9a856ee78b0eee3
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-01-05 17:37:39 +0900

    ㄴ

--- 비밀번호맞추기게임.c
+++ 비밀번호맞추기게임.c
@@ -4,35 +4,62 @@
 
 int getRandomNumber(int level);
 void showQuestion(int level, int num1, int num2);
+void success(void);
+void fail(void);
 
-
-int main(void){
-    srand(time(NULL));
-    
-    for(int i=1; i<=5; i++){
-        int num1= getRandomNumber(i);
+int main(void)
+{
+    srand((unsigned)time(NULL));
+    int count = 0;
+    for (int i = 1; i <= 5; i++)
+    {
+        // x * y = ?
+        int num1 = getRandomNumber(i);
         int num2 = getRandomNumber(i);
         showQuestion(i, num1, num2);
+
         int answer = -1;
         scanf("%d", &answer);
-        printf("----\n");
-        if(answer==num1*num2){
-            printf("맞췄습니당\n");
+        if (answer == -1)
+        {
+            printf("프로그램을 종료합니다\n");
+            exit(0);
         }
-        else{
-            printf("틀렸습니다\n");
+        else if (answer == num1 * num2)
+        {
+            success();
+            count++;
+        }
+        else
+        {
+            fail();
         }
     }
+
+    printf("\n\n 당신은 5개의 비밀번호 중 %d 개를 맞혔습니다\n", count);
+
+    return 0;
 }
 
-
-int getRandomNumber(int level){
-    int num = rand() % (level*9)+1;
-    return num; 
+int getRandomNumber(int level)
+{
+    return rand() % (level * 7) + 1;
 }
 
-void showQuestion(int level, int num1, int num2){
-    printf("%d 번째 단계\n", level);
-    printf("%d x %d ?\n", num1, num2);
-    printf("정답을 입력하세요 => (종료를 원하시면 -1 입력)\n");
+void showQuestion(int level, int num1, int num2)
+{
+    printf("\n\n\n########## %d 번째 비밀번호 ########\n", level);
+    printf("\n\t%d x %d 는?\n\n", num1, num2);
+    printf("##################################\n");
+    printf("\n비밀번호를 입력하세요 (종료 : -1) >> ");
+}
+
+void success(void)
+{
+    printf("\n >> Good ! 정답입니다 \n");
+}
+
+void fail(void)
+{
+    printf("\n >> 땡 ! 틀렸습니다 \n");
 }
