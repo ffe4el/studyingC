902ca5d6d2fb393f7023ccc55505041c71fb658a
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-01-06 20:55:54 +0900

    !!

--- .vscode/포인터로_값교환.c
+++ .vscode/포인터로_값교환.c
@@ -15,7 +15,7 @@ int main(void)
     printf("swap() 함수 호출 후 => a : %d, b : %d\n", a, b);
 
     printf("swap_addr() 함수 호출 전 => a : %d, b : %d\n", a, b);
-    swap_addr(&a, &b);
+    swap_addr(&a, &b);          // 매개변수로 포인터 함수를 전달하면, 인수로는 메모리 주소를 전달해야한다.
     printf("swap_addr() 함수 호출 후 => a : %d, b : %d\n", a, b);
 
     return 0;
@@ -34,7 +34,9 @@ void swap(int a, int b)
 }
 
 void swap_addr(int* a, int * b)
-{
+{   
+    printf("swap_addr() 함수 안에서 a의 주소 : %p\n", a);
+    printf("swap_addr() 함수 안에서 b의 주소 : %p\n", b);
     int temp = *a;
     *a = *b;
     *b = temp;
