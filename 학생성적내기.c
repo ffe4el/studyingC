a21f1902f9f2b8d06e7045e102a9a5cd5dd64d3c
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-01-06 16:30:00 +0900

    ?

--- 학생성적내기.c
+++ 학생성적내기.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 
-// int sum(score);
+int sum(int sc[]);
 
 int main(void){
     srand(time(NULL));
@@ -12,16 +12,16 @@ int main(void){
         score[i] = randomScore;
         printf("%d\n", score[i]);
     }
-    printf("%lu", sizeof(score)/sizeof(int));
-    // int a = sum(score);
-    // printf("총점은 %d 점 이고, 평균은 %d 점 입니다.", a, a/sizeof(score));
+    printf("%lu \n", sizeof(score)/sizeof(int));
+    int a = sum(score);
+    printf("총점은 %d 점 이고, 평균은 %d 점 입니다.", a, a/(sizeof(score)/sizeof(int)));
 }
 
-// int sum(score){
-//     int sum=0;
-//     for (int i = 0; i < sizeof(score); i++)    // 배열의 요소 개수만큼 반복
-//     {
-//         sum += score[i];    // sum과 배열의 요소를 더해서 다시 sum에 저장
-//     }
-//     return sum;
-// }
+int sum(int sc[]){
+    int sum=0;
+    for (int i = 0; i < sizeof(sc)/sizeof(int); i++)    // 배열의 요소 개수만큼 반복
+    {
+        sum += sc[i];    // sum과 배열의 요소를 더해서 다시 sum에 저장
+    }
+    return sum;
+}
