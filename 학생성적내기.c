c387d28920e0a05b74f9a30e8f083ababf4bc907
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-01-06 16:44:12 +0900

    오류 어찌저찌 해결..?!

--- 학생성적내기.c
+++ 학생성적내기.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 
-int sum(int sc[]);
+int sum(int sc[],int b);
 
 int main(void){
     srand(time(NULL));
@@ -12,16 +12,19 @@ int main(void){
         score[i] = randomScore;
         printf("%d\n", score[i]);
     }
-    printf("%lu \n", sizeof(score)/sizeof(int));
-    int a = sum(score);
-    printf("총점은 %d 점 이고, 평균은 %d 점 입니다.", a, a/(sizeof(score)/sizeof(int)));
+    int b = sizeof(score)/sizeof(int);
+    printf("%d \n", b);
+    float a = sum(score, b);
+    float c = a/b;
+    printf("총점은 %.0f 점 이고, 평균은 %.2f 점 입니다.", a, c);
 }
 
-int sum(int sc[]){
+int sum(int sc[], int b){
     int sum=0;
-    for (int i = 0; i < sizeof(sc)/sizeof(int); i++)    // 배열의 요소 개수만큼 반복
+    for (int i = 0; i < b; i++)    // 배열의 요소 개수만큼 반복
     {
         sum += sc[i];    // sum과 배열의 요소를 더해서 다시 sum에 저장
     }
+    printf("** %d \n", sum);
     return sum;
 }
