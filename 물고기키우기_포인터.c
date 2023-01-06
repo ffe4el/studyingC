74369d4a87f9cd81515a8e1b79344ef858f1e568
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-01-06 22:36:23 +0900

    Update 물고기키우기_포인터.c

--- 물고기키우기_포인터.c
+++ 물고기키우기_포인터.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-int arrayfish[6]={0,0,0,0,0,0};
+
+int level;
+int arrayfish[6];
+void initData();    // 게임 초기화 함수 선언
+
 
 int main(void){
-    void initData();
+    initData(); // 게임 초기화 함수 호출
+
+    return 0;
+}
+
+void initData(){
+    level=1;
+    for(int i=0; i<6; i++){
+        arrayfish[i]=100;   // 어항 물높이 초기화
+    }
+
 }
