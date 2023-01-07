4eb45f43fe8bf4e980feb59520e93221566292a8
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-01-07 15:20:25 +0900

    삼성문제1

---
+++ samsung1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    int j;
+    int arr2d[3][3];
+
+    // scanf로 데이터 입력받기
+    for(i=0; i<3;i++){
+        for (j=0; j<3; j++ ){
+            printf("arr2d[%d][%d] : ", i,j);
+            scanf("%d", &arr2d[i][j]);
+        }
+    }
+
+    for(i=0; i<3;i++){
+        for(j=0; j<3; j++ ){
+            printf("arr2d[%d][%d] : %d \n", i,j, arr2d[i][j]);
+
+        }
+    }
+}
