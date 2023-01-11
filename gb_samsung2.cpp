22715d1637580140bbe99109e423608ca1ba2467
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-01-11 15:24:28 +0900

    c++ 코드 추가

---
+++ gb_samsung2.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int decToBin(int temp){
+   int binary[9] ={0,};
+   int position =0, result=0;
+   while (1){
+      binary[position] = temp % 2;   
+      temp = temp / 2;           
+      position++;    
+      if (temp == 0) break;                      
+   }
+   for (int i = position - 1; i >= 0; i--){
+      result+= binary[i]*(pow(10,i));
+   } 
+   return result;
+}
+
+int binToResult(int binary, int x, int y){
+   int result=0;
+   for(int i=0; binary>0; i++){
+      if(binary%10 ==1) result+= y*(pow(10,i));
+      else result+= x*(pow(10,i));
+
+      binary/=10;
+   }
+   return result;
+}
+
+int main(){
+   int testCase;
+   scanf("%d", &testCase);
+
+   while(--testCase){
+      int n, x, y, result =-1;
+      
+      scanf("%d %d %d", &n, &x, &y);   
+      int cnt = 0, temp=n;
+      while(temp>0){
+         cnt++;
+         temp/10;
+      }
+      int maxNum = pow(2, cnt) -1;
+      for(int i=maxNum; i>0; i--){
+         int binary = decToBin(maxNum);
+         int result = binToResult(binary,x ,y)<n ?  binToResult(binary,x ,y): -1;
+      }
+      return result;   
+   }
+}
