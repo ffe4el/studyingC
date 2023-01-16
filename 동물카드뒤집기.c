5e92737b7349065830db3c85df8250c0a19964c7
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-01-16 14:30:27 +0900

    Update 동물카드뒤집기.c

--- 동물카드뒤집기.c
+++ 동물카드뒤집기.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int arrayAnimal[4][5];
-void initAnimalArray(void); // 배열 값 초기화
+void initAnimalArray(); // 배열 값 초기화
 char * strAnimal[10];   // 동물 이름 리스트
-void initAnimalName(void);  // 동물 이름 초기화
-
+void initAnimalName();  // 동물 이름 초기화
+void suffleAnimal();    // 동물 이름을 카드지도위에 무작위 배치
+int getEmptyPosition(); // 카드지도에서 빈공간을 찾아 위치 반환
+int conv_pos_x(int x);
+int conv_pos_y(int y);
+void printAnimal();
+void printQuestion();
+int checkAnimal[4][5];
 
 int main(){
     initAnimalArray();
     initAnimalName();   
+    suffleAnimal();
+    srand(time(NULL)); //난수 초기화
     return 0 ;
 }
 
-void initAnimalArray(void){
+void initAnimalArray(){
     for(int i=0; i<4; i++){
         for(int j=0; j<5; j++){
             arrayAnimal[i][j] = -1;     //카드 뒷면 설정
@@ -20,16 +30,75 @@ void initAnimalArray(void){
     }
 }
 
-void initAnimalName(void){
+void initAnimalName(){
     strAnimal[0] = "원숭이";
     strAnimal[1] = "하마";
     strAnimal[2] = "강아지";
     strAnimal[3] = "고양이";
     strAnimal[4] = "돼지";
-
     strAnimal[5] = "코끼리";
     strAnimal[6] = "기린";
     strAnimal[7] = "낙타";
     strAnimal[8] = "타조";
     strAnimal[9] = "호랑이";
+}
+
+void suffleAnimal(){
+    for(int i=0; i<10; i++){
+        for(int j =0; j<2; j++){
+            int pos = getEmptyPosition(); // 카드지도에서 빈공간을 찾아 위치 반환
+            int x = conv_pos_x(pos);    //pos를 x의 (4,5)배열로 변환
+            int y = conv_pos_y(pos);    //pos를 y의 (4,5)배열로 변환
+            arrayAnimal[x][y]= i;
+        }
+    }
+}
+
+int getEmptyPosition(){
+    while(1){
+        int randPos = rand() % 20;
+        int x = conv_pos_x(randPos);
+        int y = conv_pos_y(randPos);
+        if (arrayAnimal[x][y] == -1){
+        return randPos;
+        }
+    }
+    return 0;
+}
+
+int conv_pos_x(int x){
+    return x/5;
+}
+
+int conv_pos_y(int y){
+    return y%5;
+}
+
+void printAnimal(){
+    printf("\n =====동물의 위치를 보여줍니다==== \n\n");
+    for(int i=0; i<4; i++){
+        for(int j=0; j<5;j++){
+            printf("%8s", strAnimal[arrayAnimal[i][j]]);
+
+        }
+        printf("\n");
+    }
+    printf("\n==================\n");
+}
+
+void printQuestion(){
+    int seq = 0;
+     for(int i=0; i<4; i++){
+        for(int j=0; j<5;j++){
+            if(checkAnimal[i][j] != 0){     //카드가 앞면일때
+                printf("%8s", strAnimal[arrayAnimal[i][j]]);
+            }
+            else {      //카드가 뒷면일때
+                printf("%8d", seq); //카드번호 출력
+            }
+            seq++;
+        }
+        printf("\n")
+     }
+    
 }
