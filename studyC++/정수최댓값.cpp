b1d01f5a60eae96faba64e690927d625f74743ca
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-03 17:54:28 +0900

    개그튼거!

--- studyC++/정수최댓값.cpp
+++ studyC++/정수최댓값.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int big(int *arr, int n, int max_num);
+int big(int* arr, int n, int max_num);
 
 int main()
 {
@@ -9,21 +9,20 @@ int main()
     int arr[100];
     cin >> n;
     for(i = 0; i< n; i++) cin >> arr[i];
-    for(i = 0; i< n; i++) cout << arr[i];
-    cout << arr;
+    // cout << arr;
     max_num = 0;
     big(arr, n, max_num);
-    cout << max_num;
+    // cout << max_num;
     return 0;
 }
 
-int big(int *arr, int n, int max_num){
+int big(int* arr, int n, int max_num){
     max_num = arr[0];
-    int i;
-    for(i=1; i<n; i++){
-        if (arr[i+1]>arr[i]){
-            max_num = arr[i+1];
+    for(int i=1; i<n; i++){
+        if (arr[i]>max_num){
+            max_num = arr[i];
         }
     }
+    cout << max_num << endl;
     return max_num;
 }
