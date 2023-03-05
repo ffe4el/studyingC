9c82b9012ba3193be12d3d866f6bc2b980f5c6e8
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-05 17:48:20 +0900

    및히게따!

--- studyC++/백준/배열/바구니바꾸기.cpp
+++ studyC++/백준/배열/바구니바꾸기.cpp
@@ -2,27 +2,31 @@
 #include <iostream>
 using namespace std;
 
+void swap(int& a, int& b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 int main(){
     int n,m,i,j;
     cin >> n >> m;
     int arr[1001];
-    for(int a=0; a<n; a++){
-        arr[a]=a+1;
+    for(int i=0; i<n; i++){
+        arr[i]=i+1;
     }
     for(int a=0;a<m;a++){
         cin >> i >> j;
         int k;
         k = j-i;
-        for(int b=0; b<k+1; b++){
-            int time;
-            time = arr[j-1];
-            arr[i+b-1]= time-b;
-            // cout<<"*"<<arr[i+b-1]<<endl;
-            // cout<<"!"<<arr[j-b-1]<<endl;
+        for(int b=0; b<=(k)/2; b++){
+            // cout << arr[b+i];
+            swap(arr[b+i-1], arr[j-b-1]);
         }
     }
-    for(int a=0; a<n; a++){
-        cout<<arr[a]<<" ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
     }
     cout<<endl;
     return 0;
