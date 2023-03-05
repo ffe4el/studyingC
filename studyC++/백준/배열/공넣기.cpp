85fa6f4cc9857444c47aad85257aa8e084b1f5e2
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-05 16:22:03 +0900

    c++로 문제푸는중

---
+++ studyC++/백준/배열/공넣기.cpp
@@ -0,0 +1,24 @@
+//10810
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m, i, j, k;
+    cin >> n >> m;
+    int arr[101];
+    for(int a=0; a<n; a++){
+        arr[a]=0;
+    }
+    // cout << arr[2] <<endl;
+    for(int a=0; a<m; a++){
+        cin >> i >> j >> k;
+        for(int b=i-1; b<j;b++){
+            arr[b]=k;
+        }
+    }
+    for(int a=0; a<n; a++){
+        cout<<arr[a];
+    }
+    cout<<endl;
+    return 0;
+}
---
+++ studyC++/백준/배열/공바꾸기.cpp
@@ -0,0 +1,28 @@
+//10813
+#include <iostream>
+using namespace std;
+
+void swap(int* arr,int i, int j){
+    int time;
+    time = arr[i];
+    arr[i] = arr[j];
+    arr[j] = time;
+}
+
+int main(){
+    int n, m, i, j;
+    cin >> n >> m;
+    int arr[1001];
+    for(int a=0; a<n; a++){
+        arr[a]=a+1;
+    }
+    for(int a=0;a<m;a++){
+        cin >> i >> j;
+        swap(arr,i-1,j-1);
+    }
+    for(int a=0;a<n;a++){
+        cout<<arr[a];
+    }
+    cout<<endl;
+    return 0;
+}
