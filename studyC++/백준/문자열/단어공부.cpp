63038261f2399dc549c699fbe917e39f4f050583
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-06 15:45:16 +0900

    ??

---
+++ studyC++/백준/문자열/단어공부.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string a;
+    cin >> a;
+    int al;
+    al = a.length();
+    cout << al;
+    // for(int i=0; i<al; i++){
+    //     a[i]
+    // }
+}
