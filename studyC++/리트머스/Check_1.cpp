cde5ba42b88370f3e2338ac0350a0c234d16c17e
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-16 15:27:29 +0900

    C번도 풀기완료! 아 행복행ㅠㅠ

--- studyC++/리트머스/Check_1.cpp
+++ studyC++/리트머스/Check_1.cpp
@@ -2,32 +2,32 @@
 #include <string>
 // #include "ArrayStack.h"
 using namespace std;
-
+ 
 const int MAX_STACK_SIZE = 20;
-
+ 
 // inline void error(char *message){
 //     cout << message <<endl;
 //     exit(1);
 // }
-
-
+ 
+ 
 class ArrayStack
 {
     int top;    //요소 개수
     int data[MAX_STACK_SIZE]; //요소의 배열
-
+ 
 public :
     ArrayStack(){top = -1;} //스택의 생성자
     //반환자가 bool 타입이기 때문에 return top==-1일때 True를 반환하고, top!=-1이면 False를 반환한다.
     bool isEmpty(){return top == -1;}
     bool isFull(){return top == MAX_STACK_SIZE-1;}
-
+ 
     void push(int e){
         //만약 스택이 가득 찼다면
         // if(isFull()) error ("스택 포화 에러");
         data[++top] =e; //top에 먼저 1 더하고, 그 자리에 새로운 e값 넣기
     }
-
+ 
     int pop(){
         // if(isEmpty()) {
         //     error ("EOF");
@@ -35,14 +35,14 @@ public :
         //
         return data[top--];
     }
-
+ 
     int peek(){
         // if(isEmpty()) {
         //     error ("EOF");
         // }
         return data[top];
     }
-
+ 
     void display(){
         printf("[스택 항목의 수 = %2d] ==> ", top+1);
         for(int i=0; i<=top; i++){
@@ -51,7 +51,7 @@ public :
         }
     }
 };
-
+ 
 void checkMatching(string Str, int cnt){
     ArrayStack stack;
     string ans = "OK";
@@ -64,28 +64,35 @@ void checkMatching(string Str, int cnt){
         if(Str[i] == '[' || Str[i] =='(' || Str[i] =='{'){
             stack.push(Str[i]);
         }
-
+ 
         else if(Str[i] == ']' || Str[i]==')' || Str[i]=='}'){
             if(stack.isEmpty()){
                 ans = "Error";
             }
-            bc++;
-            stack.pop();
+            else{
+                int prev = stack.peek();
+                if((Str[i] == ']'&& prev !='[')||(Str[i] == ')'&& prev !='(')||(Str[i] == '}'&& prev !='{')){
+                    ans = "Error";
+                    continue;
+                }
+                bc++;
+                stack.pop();
+            }
         }
     }
-    
+     
     if (stack.isEmpty()){
         ans = "OK";
     }
     else{
         ans = "Error";
     }
-
+ 
     // cout << "괄호개수 : " <<a<<endl;
     cout << ans <<", Line_count : "<< cnt <<", bracket_count : "<<bc<<endl;
-
+ 
 }
-
+ 
 int main(){
     string Str,temp;
     int cnt;
@@ -99,7 +106,7 @@ int main(){
     }
     // cout << Str << endl;
     checkMatching(Str, cnt);
-    
-
+     
+ 
     return 0;
 }
--- studyC++/리트머스/Check_3.cpp
+++ studyC++/리트머스/Check_3.cpp
@@ -2,38 +2,91 @@
 #include <string>
 using namespace std;
 
+const int MAX_STACK_SIZE = 20;
+
+class ArrayStack
+{
+    int top;    //요소 개수
+    int data[MAX_STACK_SIZE]; //요소의 배열
+
+public :
+    ArrayStack(){top = -1;} //스택의 생성자
+    //반환자가 bool 타입이기 때문에 return top==-1일때 True를 반환하고, top!=-1이면 False를 반환한다.
+    bool isEmpty(){return top == -1;}
+    bool isFull(){return top == MAX_STACK_SIZE-1;}
+
+    void push(int e){
+        //만약 스택이 가득 찼다면
+        // if(isFull()) error ("스택 포화 에러");
+        data[++top] =e; //top에 먼저 1 더하고, 그 자리에 새로운 e값 넣기
+    }
+
+    int pop(){
+        return data[top--];
+    }
+
+    int peek(){
+        return data[top];
+    }
+
+    void display(){
+        printf("[스택 항목의 수 = %2d] ==> ", top+1);
+        for(int i=0; i<=top; i++){
+            printf("<%2d>", data[i]);
+            printf("\n");
+        }
+    }
+};
+
 void checkMatching(string Str, int cnt){
+    ArrayStack stack;
     string ans;
     int bc = 0;
     bool quotes=false;
     int a;
+    int aa;
+
     a = Str.length();
-    // cout << a<<endl;
     for(int i=0; i<a; i++){
-        if(Str[i]=='\''){
-            //그전에 \가 있었으면, 그 뒤에 '가 나오는걸 인정X
-            if(i-1>=0 && Str[i-1]=='\\'){ 
-                cout<<"zzing"<<endl;
-                continue;
-            }
-            if (quotes){    //작은따옴표닫기
-                bc++;
-                cout<<"close"<<endl;
+        if(quotes){ //큰따옴표가 열려있는 상태
+            if (Str[i]=='\"'){
                 quotes=false;
             }
-            else {  //작은따옴표 열기
-                cout<<"open"<<endl;
-                quotes = true;
+            // else{
+            //     continue;
+            // }
+        }
+        else{ //큰따옴표가 닫혀있는 상태 => ()짝맞추기
+            if(Str[i] == '[' || Str[i] =='(' || Str[i] =='{'){
+                stack.push(Str[i]);
+            }
+            else if(Str[i] == ']' || Str[i]==')' || Str[i]=='}'){
+                if(stack.isEmpty()){
+                ans = "Error";
+            }
+            else{
+                int prev = stack.peek();
+                if((Str[i] == ']'&& prev !='[')||(Str[i] == ')'&& prev !='(')||(Str[i] == '}'&& prev !='{')){
+                    ans = "Error";
+                    continue;
+                }
+                bc++;
+                stack.pop();
+            }
+            }
+            else if(Str[i]=='\"'){
+                quotes= true;
             }
         }
     }
 
-    if (quotes){
-        ans = "Error";
+    if (stack.isEmpty()){
+        ans = "OK";
     }
     else{
-        ans="OK";
+        ans = "Error";
     }
+     
 
     cout << ans <<", Line_count : "<<cnt<<", bracket_count : "<<bc<<endl;
 
