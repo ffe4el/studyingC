6d0d69495dad9e064e95ff98f58625057c96255b
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-20 10:49:39 +0900

    gd

--- studyC++/리트머스/스택/Check_5555.cpp
+++ studyC++/리트머스/스택/Check_5555.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <cstdio>
-#include <string>
-
+#include<string>
 using namespace std;
 
 const int MAX_STACK_SIZE = 20;
@@ -18,7 +16,7 @@ public :
     void push(int e){
         //만약 스택이 가득 찼다면
         // if(isFull()) error ("스택 포화 에러");
-        data[++top] =e; //top에 먼저 1 더하고, 그 자리에 새로운 e값 넣기
+        data[++top] = e; //top에 먼저 1 더하고, 그 자리에 새로운 e값 넣기
     }
     int pop(){
         return data[top--];
@@ -38,36 +36,29 @@ public :
 //객체의 우선순위 계산
 inline int precedence(char op){
     switch(op){
-        case '(' : case ')' : return 0;
         case '+' : case '-' : return 1;
         case '*' : case '/' : return 2;
     }
-    return -1; //공백처리
+    return 0;
 }
 
-void infix2Postfix(string s){
+int main(){
     ArrayStack stack;
-    string ans;
+    string ans ="";
+    string s;
+    getline(cin, s);
     for(int i=0; i<s.length(); i++){
         //피연산자 출력
         if(s[i]>='0' && s[i]<='9'){
-            if(i<=s.length()-2){
-                while(s[i]!=' '){
-                    ans += s[i];
-                    i++;
-                }
-            }
-            else if(i==s.length()-1){
+            while(s[i]!=' '){
                 ans += s[i];
+                i++;
             }
-            
-            // ans += s[i];
-            
         }
         //스택이 비어있는경우 or '('인경우 push
         else if(stack.isEmpty()|| s[i]=='('){
             stack.push(s[i]);
-            continue;
+            // continue;
         }
         //')'경우 '('만날때까지 출력
         else if(s[i]==')'){
@@ -79,7 +70,7 @@ void infix2Postfix(string s){
             continue;
         }
         //top보다 우선순위 크면 Push
-        if(precedence(stack.peek()) <= precedence(s[i])){
+        if(precedence(stack.peek()) < precedence(s[i])){
             stack.push(s[i]);
         }
         //top보다 우선순위 작거나 같으면 pop 후 반복
@@ -96,28 +87,5 @@ void infix2Postfix(string s){
         stack.pop();
     }
     cout << ans << endl;
-}
-
-int main(){
-    ArrayStack stack;
-    string ans;
-    string s;
-    double a;
-    getline(cin, s); // 입력 받기
-    //######################################################
-    // string s = ""; // 추출한 숫자를 저장할 문자열
-    // for (int i=0; i<input.length(); i++) { // 입력 문자열을 한 문자씩 검사
-    //     char c= input[i];
-    //     if (isdigit(c) || c == '.') { // 숫자나 소수점인 경우
-    //         s += c; // 문자열에 추가
-            
-    //     }
-    // }
-    // double s = stod(numStr); // 문자열을 double형으로 변환
-    //######################################################
-
-    infix2Postfix(s);
-    
-
     return 0;
 }
