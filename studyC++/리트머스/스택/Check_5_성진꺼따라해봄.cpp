9820c347609ad130cfe837e8555855776d467bce
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-21 23:22:52 +0900

    과제 끝

--- studyC++/리트머스/스택/Check_5_성진꺼따라해봄.cpp
+++ studyC++/리트머스/스택/Check_5_성진꺼따라해봄.cpp
@@ -43,3 +43,50 @@ void calPostfix(char c){
 }
 
 //중위연산자를 후위연산자로 변환
+void infix2postfix(FILE *fp= stdin){
+    char c,op;
+    double val;
+    stack <char> st;
+
+    while((c=getc(fp)) != '\n'){
+        if(c>='0' && c<='9'){
+            ungetc(c,fp);
+            fscanf(fp, "%lf", &val);
+            num.push(val); //바로 num에 push해버림
+        }
+        else if(c=='('){
+            st.push(c);
+        }
+        else if (c==')'){
+            while(!st.empty()){
+                op = st.top();
+                st.pop();
+                if(op=='(') break;  //짝 맞으면 맨 위 스택 삭제
+                else calPostfix(op); //짝이 안맞으면 계산하는 함수로 보내기
+            }
+        }
+        else if(c == '+' || c == '-' || c == '*' || c == '/'){
+            while(!st.empty()){
+                op = st.top();
+                if(precedence(c) <= precedence(op)){
+                    calPostfix(op); //c우선순위가 더 낮으면 op부터 넣어서 계산
+                    st.pop();
+                }
+                else break;
+            }
+            st.push(c);
+        }
+    }
+    while(!st.empty()){
+        calPostfix(st.top());
+        st.pop();
+    }
+
+    printf("%0.2lf\n", num.top());
+}
+
+
+
+int main(){
+    infix2postfix();
+}
