99b607fa5a816b13edd920ce383e9c54d44ed689
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-23 00:37:27 +0900

    문제풀었당~

--- studyC++/자료구조스터디/3월4째주/균형잡힌세상4949.cpp
+++ studyC++/자료구조스터디/3월4째주/균형잡힌세상4949.cpp
@@ -5,66 +5,37 @@ using namespace std;
 
 
 int main(){
-    stack <int> st;
-    string word;
-    while (word != "."){
-        
-        string word;
-        string ans="yes";
-        getline(cin, word);
-        if (word=="."){
+    while(true){ //일단 계속 입력을 받고 실행을 한다 
+        stack <char> st; // while문 안에서 초기화 시켜줘야한다.
+        string ans = "yes"; // while문 안에서 초기화 시켜줘야한다.
+        string str = ""; // while문 안에서 초기화 시켜줘야한다.
+        getline(cin, str);
+        if(str=="."){ //만약 . 이 입력된다면 exit하기
             exit(0);
         }
-        for(int i=0; i<word.length(); i++){
-            if(word[i]=='('){
-                st.push(word[i]);
-                // cout<<word[i]<<endl;
+        for(int i=0; i<str.length(); i++){
+            if(str[i]=='(' || str[i]=='['){
+                st.push(str[i]);
             }
-            else if(word[i]=='['){
-                st.push(word[i]);
-                // cout<<word[i]<<endl;
-            }
-            else if(word[i]==')' || word[i]==']'){
+            else if(str[i]==')' || str[i]==']'){
                 if(st.empty()){
                     ans = "no";
                     break;
                 }
                 else{
-                    if(word[i]==')'){
-                        if(st.top()=='('){
-                            cout << st.top()<<endl;
-                            st.pop();
-                        }
-                        else{
-                            ans = "no";
-                            break;
-                        }
-                    }
-                    else if(word[i]==']'){
-                        if(st.top()=='['){
-                            cout << st.top()<<endl;
-                            st.pop();
-                        }
-                        else{
-                            ans = "no";
-                            break;
-                        }
-                    }
+                    char prev = st.top();
+                    if((str[i] == ']'&& prev !='[')||(str[i] == ')'&& prev !='(')){
+                        ans = "no";
+                        break;
+                    }         
+                    st.pop();
                 }
-                
-            }
-            if (word[i]=='.'){
-                continue;
             }
         }
-
         if(!st.empty()){
-            ans="no";
+            ans = "no";
         }
-
-        cout << ans << endl;
+        cout<<ans<<endl;
     }
-    
-
     return 0;
 }
