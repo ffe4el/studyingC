48d5e421b1fe81822afd8d6821e917324fb53f6a
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-26 00:09:47 +0900

    흠

---
+++ studyC++/자료구조스터디/3월4째주/단어정렬.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int tc;
+    cin >> tc;
+    for(int i=0; i<tc; i++){
+        string a;
+        cin >> a; //
+        
+    }
+
+    return 0;
+}
