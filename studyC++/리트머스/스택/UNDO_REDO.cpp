fbe88b797e8435fb68fdc1b19ecccac402d5537d
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-04-03 23:21:23 +0900

    D번과제 완료!

--- studyC++/리트머스/스택/UNDO_REDO.cpp
+++ studyC++/리트머스/스택/UNDO_REDO.cpp
@@ -5,42 +5,42 @@
 using namespace std;
 
 int main(){
-    deque <char> un;
+    deque <char> un; //undo는 갯수가 10이 넘으면 앞에것을 지워야함으로 deque으로 구현
     stack <char> re;
-    int n=11;
-    bool undo=false; //undo 이후 redo가 아닌 일반데이터를 입락받으면 redo스택은 비우기
+    int  undo=0; //undo 이후 redo가 아닌 일반데이터를 입락받으면 redo스택은 비우기
     char c;
 
     //개행문자가 들어올때까지 입력받기
     do {
         cin >> c;
         if(c=='U'){
-            if(!un.empty()){
-                re.push(un.back());
+            if(!un.empty()){//un스택이 비어있지 않으면
+                re.push(un.back());//un스택에서 꺼내서 re스택에 넣기
                 un.pop_back();
             }
-            else{
+            else{//비어있으면 에러 띄우기
                 cout << "ERROR" << endl;
                 exit(0);
             }
-            undo = true;
+            undo++;//undo count
         }
         else if(c=='R'){
-            if(!re.empty()){
-                un.push_back(re.top());
+            if(!re.empty()){//re스택이 비어있지 않으면
+                un.push_back(re.top());//re스택에서 꺼내서 un스택에 넣기
                 re.pop();
             }
-            else{
+            else{//비어있으면 에러 띄우기
                 cout << "ERROR" <<endl;
                 exit(0);
             }
-            undo = false;
+            undo--;//undo상쇄
             
         }
         else{
+            //일반 문자열이 들어오면 un스택에 넣기
             un.push_back(c);
-            if(undo==true){
-                //redu 초기화
+            if(undo!=0){//undo가 모두 상쇄되지 않았다면
+                //redu 전체 초기화
                 while(!re.empty()){
                     re.pop();
                 }
@@ -52,7 +52,7 @@ int main(){
                 un.pop_front();
             }
         }
-    } while (getc(stdin) == ' ');
+    } while (getc(stdin) == ' ');//엔터가 들어올때까지 입력받기
 
     //undo 스택 출력하기
     if(un.empty()){
@@ -70,7 +70,7 @@ int main(){
 
     //redo 스택 출력하기
     if(re.empty()){
-        cout << "EMPTY" <<endl;
+        cout << "EMPTY";
     }
     else{
         while(!re.empty()){
---
+++ studyC++/리트머스/스택/undoredo_sj.cpp
@@ -0,0 +1,105 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+const int MAX_STACK_SIZE = 10;
+
+class CircularStack {
+protected:
+   int top;
+   int bottom;
+   char data[MAX_STACK_SIZE];
+public:
+   CircularStack()
+   {
+      top = -1;
+      bottom = 0;
+   }
+   bool isEmpty() { return top == bottom - 1; }
+   void push(char al)
+   {
+      if (top+1 >= MAX_STACK_SIZE)
+      {
+         top++;
+         bottom++;
+         data[top] = al;
+      }
+      else
+      {
+         top++;
+         data[top] = al;
+      }
+
+   }
+   char pop()
+   {
+      if (!isEmpty())
+      {
+         char al = data[top];
+         top--;
+         return al;
+      }
+      
+   }
+   int size()
+   {
+      return top - bottom + 1;
+   }
+};
+int main()
+{
+   char al;
+   CircularStack undo;
+   CircularStack redo;
+
+   do {
+      cin >> al;
+      if (al >='a' && al <= 'z')
+      {
+         undo.push(al);
+         while (!redo.isEmpty())
+            redo.pop();
+      }
+
+      else if (al == 'U')
+      {
+         if (undo.isEmpty())
+         {
+            cout << "ERROR " << endl;
+            return 0;
+         }
+         redo.push(undo.pop());
+
+      }
+
+      else if (al == 'R')
+      {
+         if (redo.isEmpty())
+         {
+            cout << "ERROR " << endl;
+            return 0;
+         }
+         undo.push(redo.pop());
+         
+      }
+   } while (getc(stdin) == ' '); //엔터를 입력하면 입력 반복문 탈출
+
+   if (undo.size() == 0)
+      cout << "EMPTY ";
+   while (undo.size() > 0)
+   {
+      cout << undo.pop() << " ";
+   }
+   cout << endl;
+
+   if (redo.size() == 0)
+      cout << "EMPTY ";
+   while (redo.size()>0)
+      cout << redo.pop() << " ";
+   cout<<endl;
+
+    return 0;
+
+}
