ba85e239b94072a33f5181b4d80bf824c6b64ee0
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-04-13 01:03:29 +0900

    왜 오류지

--- studyC++/리트머스/포인터&연결리스트/강의복습/LinkedStack.h
+++ studyC++/리트머스/포인터&연결리스트/강의복습/LinkedStack.h
@@ -13,18 +13,24 @@ public :
             top = p;
         }
     }
+
     Node* pop(){
         if(isEmpty()) return NULL;
+        // 공백상태가 아니면 현재의 top을 p 다음으로 연결하고 포인터 top을 새로 들어온 노드인 p로 변경함.
         Node *p = top;
         top = top -> getLink();
         return p;
     }
+
     Node* peek(){return top;}
     
     void display(){
         printf("[LinkedStack]\n");
+        
+        // 임시 포인터 변수 p를 선언하고 top을 복사한 후 루프가 반복될 때 마다 p가 연결 리스트의 다음 노드를 가리키도록 해서 
+        // 모든 노드를 한 번 씩 방문할 수 있도록 함. 
         for(Node *p = top; p!=NULL; p=p->getLink())
-            p->display();
+            // p->display(); ? 이거 왜 오류?
         printf( "\n");
     }
 
